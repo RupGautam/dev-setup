063611ad2290e3859d0a1e22cbd7dad11a65374a
Rup Gautam <RupGautam@Rup-iMac.local>
2015-12-14 21:25:35 -0600

    powerline fonts added

--- custom.sh
+++ custom.sh
@@ -1,4 +1,13 @@
 # Most have CLIs'
 brew install linode-cli #manage linode nodes from terminal
 npm install -g cloudflare-cli #manage domain names from terminal
-gem install tugboat #manage DO droplets from terminal 
+gem install tugboat #manage DO droplets from terminal
+
+# Powerline fonts
+git clone https://github.com/powerline/fonts.git && cd fonts
+sudo chmod +x install.sh && ./install.sh
+sudo rm -rf fonts
+
+
+
+
