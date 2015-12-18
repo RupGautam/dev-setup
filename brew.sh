4d6a2e27ec449eb2425c262053fa0ba3cb095bce
Rup Gautam <RupGautam@users.noreply.github.com>
2015-12-17 23:05:08 -0600

    npm packages| font packages | cocoapods and powerline font added

--- brew.sh
+++ brew.sh
@@ -141,21 +141,43 @@ brew cask install --appdir="/Applications" namechanger
 brew cask install --appdir="/Applications" sketch
 brew cask install --appdir="/Applications" microsoft-office
 
+# Most have CLIs'
+brew install linode-cli #manage linode nodes from terminal
+npm install -g cloudflare-cli #manage domain names from terminal
+gem install tugboat #manage DO droplets from terminal
 
+# Powerline fonts
+git clone https://github.com/powerline/fonts.git && cd fonts
+sudo chmod +x install.sh && ./install.sh
+sudo rm -rf fonts
+# Install fonts
+brew tap caskroom/fonts
+brew update && brew cleanup
+brew cask install font-roboto
+brew cask install font-roboto-slab
+brew cask install font-ubuntu
+brew cask install font-anonymous-pro
+brew cask install font-droid-sans
+#brew cask install font-droid-sans-mono
+#brew cask install font-droid-serif
+#brew cask install font-nexa
+brew cask install font-open-sans
+#brew cask install font-open-sans-condensed
+brew cask install font-source-code-pro
 
+# Install npm packages
+sudo npm install -g ios-sim
+sudo npm install -g yo
+sudo npm install -g generator-angular-fullstack
+sudo npm install -g grunt-cli
+sudo npm install -g bower
 
-#brew cask install --appdir="/Applications" gimp
-#brew cask install --appdir="/Applications" inkscape
+# Install gems
+sudo gem install cocoapods
+sudo gem install cocoapods-keys
 
-#Remove comment to install LaTeX distribution MacTeX
-#brew cask install --appdir="/Applications" mactex
-
-# Link cask apps to Alfred
-#brew cask alfred link
-
-# Install Docker, which requires virtualbox
-#brew install docker
-#brew install boot2docker
+# Install Xcode package manager
+curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
